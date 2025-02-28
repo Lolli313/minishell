adbd2a134cfdc718960e5404f2a10b33913d3a9a
Francois Patrick Mick <fmick@c2r5s4.42luxembourg.lu>
2025-02-28 11:06:22 +0100

    workworkworkworkworkwork

--- src/ft_interactive_mode.c
+++ src/ft_interactive_mode.c
@@ -6,12 +6,19 @@
 /*   By: fmick <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 10:31:20 by fmick             #+#    #+#             */
-/*   Updated: 2025/02/28 10:32:02 by fmick            ###   ########.fr       */
+/*   Updated: 2025/02/28 10:53:24 by fmick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	g_global;
+
 // TODO
 // ctrl-C ctrl-D ctrl-
-// SIGNALS -> kill signal
+// SIGNALS -> kill signali
+//
+// void ft_TODO(TODO)
+{
+	// kill->signal
+}
